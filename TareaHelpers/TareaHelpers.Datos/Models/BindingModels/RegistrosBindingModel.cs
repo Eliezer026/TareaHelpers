957d83bdc72ef51934d8e7e3859f492cb8ddb607
Eliezer026 <macgiver018@gmail.com>
2019-03-24 16:57:01 -0400

    Tareadeloshelpers
    tarea de subir el archivo a github

---
+++ TareaHelpers/TareaHelpers.Datos/Models/BindingModels/RegistrosBindingModel.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TareaHelpers.Datos.Models.BindingModels
+{
+
+   
+
+    public class RegistroUsuariosBindingModel
+    {
+        /*[Range(10,11, ErrorMessage ="debe de ser de 11 caracteres")]*/
+        [Required(ErrorMessage ="Introduzca una CEDULA y que solo sea numeros por favor")]
+        [Display(Name = "Cedula")]
+        public long Cedula { get; set; }
+
+        [Required(ErrorMessage = "Introduzca un NOMBRE por favor")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Introduzca un APELLIDO por favor")]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+
+
+        [Required(ErrorMessage = "Introduzca un GENERO por favor")]
+        [Display(Name = "Sexo")]
+        public Gender Sexo { get; set; }
+
+
+        [Required(ErrorMessage = "Introduzca una ocupacion por favor")]
+        [Display(Name = "Ocupacion")]
+        public string Ocupacion { get; set; }
+
+
+        [Required(ErrorMessage = "Introduzca un HOBBY por favor")]
+        [Display(Name = "Hobby")]
+        public bool CheckBox { get; set; }
+
+        [Required(ErrorMessage ="debe de poner un correo")]
+        [Display(Name ="Correo")]
+        [EmailAddress(ErrorMessage ="Este correo no es valido, ponga un correo que sea valido")]
+        public string Correo { get; set; }
+
+        [Display(Name ="Edad")]
+        [Required(ErrorMessage ="debe de poner su edad")]
+        [Range(15,99, ErrorMessage ="Debe de tener mas de 15")]
+        public long Edad { get; set; }
+
+
+        [Display(Name ="Telefono")]
+        [Required(ErrorMessage ="ponga su telefono")]
+        
+        [DataType(DataType.PhoneNumber, ErrorMessage ="Ponga un numero de 10 digito")]
+        public long Telefono { get; set; }
+
+        
+
+       
+
+
+
+    }
+
+  
+
+
+    public enum Gender
+    {
+        Hombre,
+        Mujer
+        
+    }
+
+
+    
+
+
+}
---
+++ TareaHelpers/TareaHelpers/Controllers/RegistrosController.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using TareaHelpers.Datos;
+
+
+
+namespace TareaHelpers.Controllers
+{
+    public class RegistrosController : Controller
+    {
+        // GET: Registros
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+       
+        public ActionResult Registro()
+        {
+
+            return View();
+
+        }
+
+        
+        [HttpPost]
+        public ActionResult Registro(Datos.Models.BindingModels.RegistroUsuariosBindingModel model, string correr, string bailar, string cantar, string pintar)
+        {
+
+            
+            ViewBag.Nombre = model.Nombre;
+            ViewBag.Apellido = model.Apellido;
+            ViewBag.Cedula = model.Cedula;
+            ViewBag.Sexo = model.Sexo;
+            ViewBag.Ocupacion = model.Ocupacion;
+            ViewBag.CheckBox = model.CheckBox;
+            ViewBag.Correo = model.Correo;
+            ViewBag.Edad = model.Edad;
+            ViewBag.Telefono = model.Telefono;
+
+            if (correr == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Correr";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+            if (bailar == "true" && correr == "true")
+            {
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Bailar, Cantar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+            }
+            if(bailar == "true")
+            {
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Bailar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+            }
+            ////////////////////////////////////////////////////////////////
+
+
+
+            if (cantar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Cantar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+
+            if (pintar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+
+            if (cantar == "true" && pintar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar, Cantar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+
+            //////////////////////////////////////////////////////
+
+
+            if (cantar == "true" && pintar == "true" && correr == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar Cantar Correr";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            if (cantar == "true" && pintar == "true" && bailar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar Cantar Bailar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            if (cantar == "true" && pintar == "true" && correr == "true" && bailar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar Cantar Correr bailar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            //////////////////////////////////////////////////////////////////////
+
+
+            if (cantar == "true" && correr == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Cantar, Correr";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            if (cantar == "true" && bailar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Cantar, Bailar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+            if (pintar == "true" && correr == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar, Correr";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+            if (pintar == "true" && bailar == "true")
+            {
+
+                model.CheckBox.CompareTo(true);
+                ViewBag.CheckBox = "Pintar, Bailar";
+            }
+            else
+            {
+                model.CheckBox.CompareTo(false);
+
+
+            }
+
+
+
+
+
+            /*ViewBag.Nombre = gua.Nombre;*/
+
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado");
+            }
+
+
+            return View(model);
+
+        }
+
+       /* public ActionResult Resultado(Datos.Models.BindingModels.RegistroUsuariosBindingModel ver)
+        {
+           
+            
+
+           
+
+            return View("Resultado");
+        }*/
+
+    }
+
+    internal class NombreCheckBox
+    {
+    }
+}
